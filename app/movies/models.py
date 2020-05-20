20f1a5cccc15b60a6430a31b9327374ef1164352
Babak Nikanjam <babak.nikanjam@gmail.com>
2020-05-19 22:46:27 -0700

    Model test

--- app/movies/models.py
+++ app/movies/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    genre = models.CharField(max_length=255)
+    year = models.CharField(max_length=4)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.title}"
--- app/movies/tests.py
+++ app/movies/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+from .models import Movie
+
+
+@pytest.mark.django_db  # explicitly request database access
+def test_movie_model():
+    movie = Movie(title="Raising Arizona", genre="comedy", year="1987")
+    movie.save()
+    assert movie.title == "Raising Arizona"
+    assert movie.genre == "comedy"
+    assert movie.year == "1987"
+    assert movie.created_date
+    assert movie.updated_date
+    assert str(movie) == movie.title

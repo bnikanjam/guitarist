7b2dc2ddd91ec033276c614d34040115161c22f0
Babak Nikanjam <babak.nikanjam@gmail.com>
2020-05-22 21:09:56 -0700

    Model test - Admin

--- app/movies/tests.py
+++ app/movies/tests.py
@@ -5,11 +5,18 @@ from .models import Movie
 
 @pytest.mark.django_db  # explicitly request database access
 def test_movie_model():
+
+    def _assert_set():
+        assert movie.title == "Raising Arizona"
+        assert movie.genre == "comedy"
+        assert movie.year == "1987"
+        assert movie.created_date
+        assert movie.updated_date
+        assert str(movie) == movie.title
+
     movie = Movie(title="Raising Arizona", genre="comedy", year="1987")
     movie.save()
-    assert movie.title == "Raising Arizona"
-    assert movie.genre == "comedy"
-    assert movie.year == "1987"
-    assert movie.created_date
-    assert movie.updated_date
-    assert str(movie) == movie.title
+    _assert_set()
+
+    movie = Movie.objects.create(title="Raising Arizona", genre="comedy", year="1987")
+    _assert_set()
